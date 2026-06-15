620a99c0d809435e3850c2c4f23ac826a3e80f49
facei <anushkapokhriyal0408@gmail.com>
2026-06-15 15:41:24 +0530

    Initial commit: InsightIQ app

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import sys, platform
+if platform.system() == "Linux":
+    try:
+        __import__('pysqlite3')
+        sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+    except ImportError:
+        pass
 import pandas as pd
 import os
 import torch
